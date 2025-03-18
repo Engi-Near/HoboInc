d5a597c79e50c55e2947acf1f3ffe60cf309470b
Engi-Near <162895313+Engi-Near@users.noreply.github.com>
2025-03-18 12:45:31 -0400

    Update UpgradeSystem.js

--- js/UpgradeSystem.js
+++ js/UpgradeSystem.js
@@ -1,17 +1,17 @@
 class UpgradeSystem {
     constructor() {
         this.upgrades = [
-            /*{
+            {
                 name: "Damage Up",
                 description: "Increase damage by 25%",
                 effects: [{ type: "damageMultiplier", value: 1.25 }]
-            },*/
+            },
             {
                 name: "Speed Demon",
                 description: "Increase movement speed by 20%",
                 effects: [{ type: "speedMultiplier", value: 1.2 }]
             },
-            /*{
+            {
                 name: "Rapid Fire",
                 description: "Increase fire rate by 25%",
                 effects: [{ type: "fireRateMultiplier", value: 1.25 }]
@@ -20,13 +20,13 @@ class UpgradeSystem {
                 name: "Quick Recovery",
                 description: "Reduce health recovery time by 30%",
                 effects: [{ type: "healthRecoveryMultiplier", value: 0.7 }]
-            },*/
+            },
             {
                 name: "Critical Strike",
                 description: "Enable critical hits (10% chance, 2x damage)",
                 effects: [{ type: "enableCritical" }]
             },
-            /*{
+            {
                 name: "Critical Mastery",
                 description: "Increase critical damage by 50%",
                 effects: [{ type: "criticalDamageMultiplier", value: 1.5 }]
@@ -40,12 +40,12 @@ class UpgradeSystem {
                 name: "Magnetic",
                 description: "Increase pickup range by 50%",
                 effects: [{ type: "pickupRangeMultiplier", value: 1.5 }]
-            },*/
+            },
             {
                 name: "Shield",
                 description: "Gain a protective shield",
                 effects: [{ type: "gainShield" }]
-            }/*,
+            },
             {
                 name: "Shotgun",
                 description: "Switch to Shotgun",
@@ -63,7 +63,7 @@ class UpgradeSystem {
                     { type: "damageMultiplier", value: 1.2 },
                     { type: "criticalChanceMultiplier", value: 1.3 }
                 ]
-            }*/
+            }
         ];
     }
 
